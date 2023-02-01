33beced46cb5c48c70382cc5c099d2a9223a4fe7
Anik <A.N.I.K@live.com>
2023-02-01 08:07:00 -0500

    Partitioning Algorithm

--- tree_partitioning_algorithm.py
+++ tree_partitioning_algorithm.py
@@ -4,19 +4,22 @@ Created on Jan 30, 2023
 @author: ANIK
 '''
 
+from test.test_print import TestPrint
 import ast
+import random
 
 from AlgebraicExpressionParser import ExpressionParser
 from AlgebraicExpressionParser import Operator
 from AlgebraicExpressionParser import Operators
-
 from treelib import Node, Tree
 
 # binaryOperators = ['+', '-', '*', '^']
 # unaryOperators = ['sin']
-
 binaryOperators = ['A', 'O', 'U', 'R']
 unaryOperators = ['G', 'F', '!']
+
+qMat = []
+qMat.append([-1])
 
 # def addNodeRec(selfID, parentID, synTree, expTree):
 #
@@ -200,8 +203,8 @@ def genSynTree(exp):
     selfID = [0]
     selfVar = [0]
 
-    qMat = []
-    qMat.append([-1])
+    # qMat = []
+    # qMat.append([-1])
 
     addNodeRec(selfID, 0, selfVar, -1, synTree, expTree, qMat, -1)
 
@@ -220,6 +223,159 @@ def getNodeData(synTree, nodeID):
     return synTree[nodeID].tag
 
 
+def getNodeInterval(synTree, nodeID):
+
+    tag = getNodeData(synTree, nodeID)
+    start = (tag[tag.find('[') + 1: tag.find(',')])
+    end = (tag[tag.find(',') + 2: tag.find(']')])
+
+    return start, end
+
+
+def randomizeID(synTree):
+
+    synNodes = synTree.all_nodes()
+    # synList = []
+
+    for i in range(len(synNodes)):
+
+        nodeString = ('{}'.format(synNodes[i]))
+        # synList.append(nodeString[nodeString.find('identifier=') + 11:nodeString.find(', data')])
+
+        # print(nodeString[nodeString.find('identifier=') + 11:nodeString.find(', data')])
+
+        nodeID = 3
+
+        print(synTree.get_node(nodeID).identifier)
+
+    print(synNodes)
+
+
+def getSubTree(synTree, nodeID):
+
+    subTree = Tree()
+
+    currentID = '{}'.format(subTree.create_node(getNodeData(synTree, nodeID), random.randrange(100, 1000000)))
+    currentID = int(currentID[currentID.find('identifier=') + 11:currentID.find(', data=')])
+
+    def copyNodeRec(synTree, nodeID, parentID):
+
+        if not synTree.get_node(nodeID).is_leaf():
+
+            childrenList = synTree.children(nodeID)
+
+            for child in childrenList:
+
+                currentID = '{}'.format(child)
+                currentID = '{}'.format(subTree.create_node(currentID[currentID.find('tag=') + 4:currentID.find(', identifier=')], random.randrange(100, 1000000), parent = parentID))
+                currentID = int(currentID[currentID.find('identifier=') + 11:currentID.find(', data=')])
+
+                nodeID = '{}'.format(child)
+                nodeID = int(nodeID[nodeID.find('identifier=') + 11:nodeID.find(', data=')])
+
+                # print(synTree.all_nodes())
+                # print(nodeID, currentID)
+
+                copyNodeRec(synTree, nodeID, currentID)
+
+    copyNodeRec(synTree, nodeID, currentID)
+
+    # print(subTree.all_nodes())
+
+    return subTree
+
+
+def treePartition(synTree, qMat, nodeID, time):
+
+    if nodeID == -1 and qMat[0][1] == 0:
+
+        newRoot = Tree()
+        tag = synTree.get_node(0).tag
+
+        if 'FORALL' in tag:
+
+            rootID = newRoot.create_node('AND', random.randrange(100, 1000000)).identifier
+
+        if 'EXISTS' in tag:
+
+            rootID = newRoot.create_node('OR', random.randrange(100, 1000000)).identifier
+
+        # subTreeOne = getSubTree(synTree, 0)
+        subTreeTwo = getSubTree(synTree, 0)
+
+        # newRoot.paste(rootID, subTreeOne)
+        newRoot.paste(rootID, subTreeTwo)
+
+        synTree.get_node(0).tag = '{}{})'.format(tag[:tag.find(',') + 2], time)
+        subTreeTwo.get_node(subTreeTwo.root).tag = '{}{}{}'.format(tag[:tag.find('[') + 1], time, tag[tag.find(','):])
+
+        for i in range(len(qMat[1]) - 1):
+
+            partNode(synTree, qMat[1][i + 1], time)
+            treePartition(synTree, qMat, qMat[1][i + 1], time)
+
+        subTreeOne = getSubTree(synTree, 0)
+        newRoot.paste(rootID, subTreeOne)
+
+        synTree = newRoot
+        return synTree
+
+    else:
+
+        for j in range(len(qMat)):
+
+            if qMat[j][0] == nodeID and len(qMat[j]) > 1:
+
+                print(j, len(qMat[j]))
+
+                for i in range(len(qMat[j]) - 1):
+
+                    # print(qMat[j][i + 1])
+
+                    partNode(synTree, qMat[j][i + 1], time)
+                    treePartition(synTree, qMat, qMat[j][i + 1], time)
+
+
+def partNode(synTree, nodeID, time):
+    
+    parentID = synTree.parent(nodeID).identifier
+    tag = synTree.get_node(nodeID).tag
+
+    # parentNode = synTree.parent(nodeID)
+    
+    if 'FORALL' in tag:
+
+        newParentID = synTree.create_node('AND', random.randrange(100, 1000000), parent = parentID).identifier
+        synTree.move_node(nodeID, newParentID)
+
+        subTree = getSubTree(synTree, nodeID)
+        subTree.get_node(subTree.root).tag = '{}{}{}'.format(tag[:tag.find('[') + 1], time, tag[tag.find(','):])
+
+        synTree.paste(newParentID, subTree)
+        
+        synTree.get_node(nodeID).tag = '{}{})'.format(tag[:tag.find(',') + 2], time)
+
+        # synTree.get_node(nodeID).tag = '{}'
+
+        # synTree.show()
+
+    if 'EXISTS' in tag:
+
+        newParentID = synTree.create_node('OR', random.randrange(100, 1000000), parent = parentID).identifier
+        synTree.move_node(nodeID, newParentID)
+
+        subTree = getSubTree(synTree, nodeID)
+        subTree.get_node(subTree.root).tag = '{}{}{}'.format(tag[:tag.find('[') + 1], time, tag[tag.find(','):])
+
+        synTree.paste(newParentID, subTree)
+
+        synTree.get_node(nodeID).tag = '{}{})'.format(tag[:tag.find(',') + 2], time)
+
+        # synTree.get_node(nodeID).tag = '{}'
+
+        # synTree.show()
+
+
 def printMatrix(qMat):
 
     testPrint = "";
@@ -235,6 +391,8 @@ def printMatrix(qMat):
         print(testPrint)
         testPrint = ""
 
+    print()
+
 
 def main():
 
@@ -244,6 +402,7 @@ def main():
     # exp = 'G[a, b](F[a, b] A)'
     # exp = 'F[a, b](F[a, b](G[a, b]p) AND G[a, b] !(q OR G[a, b]r))'
     exp = 'F[0, 10](p AND G[0, 5] !q)'
+    # exp = 'F[0, 10]p'
     
     # tr = parser.postfix(exp)
     # tr = parser.syntax_tree(exp).preorder()
@@ -254,6 +413,13 @@ def main():
     # print(parser.syntax_tree(exp).postorder())
 
     synTree = genSynTree(exp)
+    # synTree.show()
+
+    # print(getNodeInterval(synTree, 3))
+
+    # partNode(synTree, 3, 5)
+
+    synTree = treePartition(synTree, qMat, -1, 5)
     synTree.show()
 
     # print(getNodeData(synTree, 0))
